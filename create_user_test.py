b53868d92ee1ddc84798b10192867367b4ab54e2
klim <klim199608@icloud.com>
2024-03-19 18:18:56 +0300

    My first commit

---
+++ create_user_test.py
@@ -0,0 +1,17 @@
+import sender_stand_request
+import data
+
+def get_user_body(first_name):
+    current_body = data.user_body.copy()
+    current_body["firstName"] = first_name
+    return current_body
+
+def test_create_user_2_letter_in_first_name_get_success_response():
+    user_body = get_user_body("Аа")
+    user_response = sender_stand_request.post_new_user(user_body)
+
+    assert user_response.status_code == 201
+    assert user_response.json()["authToken"] != ""
+
+
+C:\Users\User\yandex_api_stand_tests
